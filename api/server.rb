35f439bc8de2da16806af4f23e03ba63cb5b5718
Brandon <punk@digitalrogues.com>
2016-02-02 19:39:28 -0800

    api works111

---
+++ api/server.rb
@@ -0,0 +1,23 @@
+require 'sinatra'
+require 'ap'
+require 'mongo'
+require 'json'
+
+
+def getRecord()
+client = Mongo::Client.new([ 'northrend.digitalrogues.com:27017' ], :database => 'magicAPI', :user => 'magic', :password => 'tech0410')
+client[:magicObj].find().limit(1).each do |document|
+    #=> Yields a BSON::Document.
+    pretty  = JSON.pretty_generate(document)
+    puts pretty
+    return pretty
+    end
+end
+
+
+get '/' do
+
+    "<html><pre>#{getRecord()}</pre></html>"
+
+
+end
