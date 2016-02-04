3abf314bdfaab23bf062af3edb96366e2c4e3347
Brandon <punk@digitalrogues.com>
2016-02-03 21:55:41 -0800

    added sorting to api

--- api/server.rb
+++ api/server.rb
@@ -6,7 +6,7 @@ set :bind, '0.0.0.0'
 
 def getRecord()
 client = Mongo::Client.new([ 'northrend.digitalrogues.com:27017' ], :database => 'magicAPI', :user => 'magic', :password => 'tech0410')
-client[:magicObj].find().limit(1).each do |document|
+client[:magicObj].find().sort(:lastUpdated_unix => -1).limit(1).each do |document|
     #=> Yields a BSON::Document.
     pretty  = JSON.pretty_generate(document)
     puts pretty
