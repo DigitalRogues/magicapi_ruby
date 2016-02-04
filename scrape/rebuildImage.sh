ab093b961dd5a1560290ff8ef36b2c873cb61eae
Brandon <punk@digitalrogues.com>
2016-02-03 21:54:17 -0800

    fix up docker files for alpine to include tzdata for right timezone

--- scrape/rebuildImage.sh
+++ scrape/rebuildImage.sh
@@ -4,5 +4,5 @@ docker rm rubyscrape
 docker rmi cawaker/magicscrape-ruby:0.1
 
 docker build -t cawaker/magicscrape-ruby:0.1 .
-docker run --name rubyscrape  -d cawaker/magicscrape-ruby:0.1
+docker run --name rubyscrape  -d  cawaker/magicscrape-ruby:0.1
 
