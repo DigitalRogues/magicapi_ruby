70fb19f26df3d4ec0d787505933670a01b4d3a85
Brandon <punk@digitalrogues.com>
2016-02-05 10:31:30 -0800

    add restart always to api

--- api/rebuildAPI.sh
+++ api/rebuildAPI.sh
@@ -5,4 +5,4 @@ docker rm magicapi
 docker rmi cawaker/magicapi:0.1
 
 docker build  -t cawaker/magicapi:0.1 .
-docker run -d --name magicapi -p 80:4567 cawaker/magicapi:0.1
+docker run -d --name magicapi --restart=always -p 80:4567 cawaker/magicapi:0.1
